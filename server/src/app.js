fea81e9d70a75f637f80eeeb897b3e791718821c
Vijesh0405 <kvijesh0405@gmail.com>
2024-01-11 21:51:47 +0530

    cors origin changed

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 //middlewares
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN && "http://localhost:5173",
+    origin: process.env.CLIENT_URL||"http://localhost:5173",
     credentials:true
 }))
 app.use(express.json({limit:"50kb"}))
